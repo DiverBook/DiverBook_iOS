3c9af7050fd88ac3f65360db5cfe674f8dcd1e2f
keon22han <keonheehan@hansung.ac.kr>
2025-04-10 16:18:33 +0900

    [feat] IDCardScanner View, ViewModel 파일 추가
    IDCardScanner View, ViewModel 파일을 추가하였습니다.

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/IDCardScannerScene/View/IDCardScannerView.swift
@@ -0,0 +1,12 @@
+//
+//  IDCardScannerView.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/10/25.
+//
+
+import SwiftUI
+
+struct IDCardScannerView: View {
+    @Bindable var store: StoreOf<>
+}
---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/IDCardScannerScene/ViewModel/IDCardScannerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  IDCardScannerViewModel.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/10/25.
+//
+
+import Combine
+
+class IDCardScannerViewModel: {
+    
+}
