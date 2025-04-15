6ef06ec9cf4aaf51768499aabcfb4b26cd1de175
keon22han <keonheehan@hansung.ac.kr>
2025-04-15 17:01:19 +0900

    [feat] TabView 구현
    메인 으로 사용할 TabView를 구현하였습니다.
    - TabType (enum) 을 TabView의 tag로 사용합니다.
        - 탐색 뷰 관련 Type은 탭뷰에 포함되지 않아 enum 타입에 포함되어 있지 않습니다!

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/DiverBookTabScene/View/DiverBookTabView.swift
@@ -0,0 +1,35 @@
+//
+//  DiverBookTabView.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/15/25.
+//
+
+import SwiftUI
+
+struct DiverBookTabView: View {
+    @StateObject var viewModel: DiverBookTabViewModel
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            TabView(selection: self.$viewModel.state.selectedTab) {
+                // MARK: TabType에 따라 Tag 부착
+                MainView(viewModel: MainViewModel(coordinator: self.viewModel.coordinator))
+                    .tag(TabType.diverBook)
+                
+                // MARK: 탐색 뷰는 탭 아이템 누를 시 탭 뷰로 보여지지 않고 새로운 뷰로 Navigating
+                
+                Text("setting")
+                    .tag(TabType.setting)
+            }
+            .tabViewStyle(.page)
+            DiverBookTabBar(selectedTab: self.$viewModel.state.selectedTab)
+        }
+        .ignoresSafeArea(edges: [.bottom])
+    }
+}
+
+#Preview {
+    @Previewable @StateObject var coordinator = Coordinator()
+    DiverBookTabView(viewModel: DiverBookTabViewModel(coordinator: coordinator))
+}
---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/DiverBookTabScene/ViewModel/DiverBookTabViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  DiverBookTabViewModel.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/15/25.
+//
+
+import Combine
+import SwiftUI
+
+class DiverBookTabViewModel: ViewModelable {
+    struct State {
+        var selectedTab: TabType = .diverBook
+    }
+    
+    enum Action {
+        
+    }
+    
+    @Published var state: State = State()
+    @ObservedObject var coordinator: Coordinator
+    
+    init(coordinator: Coordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func action(_ action: Action) {
+        
+    }
+}
