4574328096341e31806eeca28dc20cbdbcd136ab
keon22han <keonheehan@hansung.ac.kr>
2025-04-17 19:19:56 +0900

    [refactor] 뷰 관련 코드 리팩토링 진행
    뷰 관련 코드 리팩토링 진행하였습니다.

--- DiverBook_iOS/DiverBook_iOS/Sources/Presentation/DiverBookTabScene/View/SubView/DiverBookTabBar.swift
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/DiverBookTabScene/View/SubView/DiverBookTabBar.swift
@@ -13,59 +13,8 @@ struct DiverBookTabBar: View {
 
     var body: some View {
         ZStack {
-            HStack {
-                Spacer().frame(width: 60)
-
-                Button(action: {
-                    self.selectedTab = .diverBook
-                }, label: {
-                    VStack(spacing: 0) {
-                        Spacer().frame(height: 10)
-                        Image(systemName: "book").resizable().aspectRatio(
-                            contentMode: .fit
-                        ).frame(width: 25).foregroundStyle(
-                            self.selectedTab == .diverBook ? .blue : .black
-                        )
-                        .padding(.bottom, 5)
-                        Text("도감").font(.system(size: 12))
-                            .foregroundStyle(
-                                self.selectedTab == .diverBook ? .blue : .black)
-                        Spacer()
-                    }
-                })
-                .buttonStyle(.plain)
-
-                Spacer()
-
-                Button(action: {
-                    self.selectedTab = .setting
-                }, label: {
-                    VStack(spacing: 0) {
-                        Spacer().frame(height: 10)
-                        Image(systemName: "gearshape").resizable().aspectRatio(
-                            contentMode: .fit
-                        ).frame(width: 25)
-                            .padding(.bottom, 3)
-                            .foregroundStyle(
-                                self.selectedTab == .setting ? .blue : .black)
-                        Text("설정").font(.system(size: 12))
-                            .foregroundStyle(
-                                self.selectedTab == .setting ? .blue : .black)
-                        Spacer()
-                    }
-                })
-                .buttonStyle(.plain)
-
-                Spacer().frame(width: 60)
-            }
-            .frame(height: 70)
-            .background(.white)
-            .shadow(color: .blue.opacity(0.1), radius: 10, y: -5)
-            VStack {
-                Image("search").resizable().aspectRatio(contentMode: .fit)
-                    .frame(width: 80)
-                Spacer().frame(height: 30)
-            }
+            TabInsideButtons(selectedTab: self.$selectedTab)
+            SearchTabButton()
         }
     }
 }
