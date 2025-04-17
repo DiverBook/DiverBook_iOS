10cbfcdd56143494685f6c4b7ae3be12c69db881
keon22han <keonheehan@hansung.ac.kr>
2025-04-17 18:11:51 +0900

    [feat] coordinator path 추가
    네비게이팅에 필요한 path 추가하였습니다.

--- DiverBook_iOS/DiverBook_iOS/Sources/Application/DiverBookIOSApp.swift
+++ DiverBook_iOS/DiverBook_iOS/Sources/Application/DiverBookIOSApp.swift
@@ -3,23 +3,31 @@ import SwiftUI
 @main
 struct DiverBookIOSApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+
     @StateObject var coordinator: Coordinator = Coordinator()
-    
+
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: self.$coordinator.path) {
-                OnboardingView()
-                    .navigationDestination(for: Path.self, destination: { path in
-                        switch path {
-                        case .splash:
-                            OnboardingView()
-                                .toolbar(.hidden, for: .navigationBar)
-                        case .mainTab:
-                            DiverBookTabView(viewModel: DiverBookTabViewModel(coordinator: self.coordinator))
-                                .toolbar(.hidden, for: .navigationBar)
-                        }
-                    })
+                OnboardingView(coordinator: self.coordinator)
+                    .navigationDestination(
+                        for: Path.self,
+                        destination: { path in
+                            switch path {
+                            case .splash:
+                                OnboardingView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .userProfileSetting(let nickname):
+                                Text(nickname)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .idCardScan:
+                                IDCardScannerView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .mainTab:
+                                DiverBookTabView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            }
+                        })
             }
         }
     }
