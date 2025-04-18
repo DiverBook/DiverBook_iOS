b93cd12db91f274ade1dc0abd7e12b804bbf68d9
keon22han <keonheehan@hansung.ac.kr>
2025-04-19 00:20:14 +0900

    [refactor] 초기 유저 프로필 설정 뷰 중복 코드 함수로 묶기, 불필요한 코드 제거
    초기 유저 프로필 설정 뷰 중복 코드 함수로 묶기
    
    ``` Swift
    @ViewBuilder
        private func infoField(
            title: String,
            view: some View
        ) -> some View {
            Spacer().frame(height: 38)
            Text(title).font(DiveFont.bodyMedium1)
            Spacer().frame(height: 10)
            view
        }
    ```
    - ViewBuilder 를 사용해 VStack 을 한번 더 사용하지 않도록 하였습니다.
    
    불필요한 코드 제거
    - 불필요한 다음의 코드를 제거하였습니다.
    ``` Swift
    .onChange(of: self.viewModel.state.shortBio) {
    
    }
    ```

--- DiverBook_iOS/DiverBook_iOS/Sources/Presentation/UserProfileSettingScene/View/SubView/InputUserDetailInfoView.swift
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/UserProfileSettingScene/View/SubView/InputUserDetailInfoView.swift
@@ -9,55 +9,67 @@ import Combine
 import SwiftUI
 
 struct InputUserDetailInfoView: View {
-    
+
     @ObservedObject var viewModel: UserProfileSettingViewModel
-    
+
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Spacer().frame(height: 32)
+
             if self.viewModel.state.profileSettingPhase.phaseStep > 5 {
                 Text("한 줄 소개")
                 Spacer().frame(height: 10)
                 InputInfoView(
                     userInfoType: .shortBio,
                     inputText: self.$viewModel.state.shortBio)
-                .onChange(of: self.viewModel.state.shortBio) {
-                    
-                }
             }
             if self.viewModel.state.profileSettingPhase.phaseStep > 4 {
-                Spacer().frame(height: 38)
-                Text("자주 가는 장소")
-                Spacer().frame(height: 10)
-                InputInfoView(
-                    userInfoType: .preferredPlaces,
-                    inputText: self.$viewModel.state.preferredPlaces)
+                infoField(
+                    title: "자주 가는 장소",
+                    view: InputInfoView(
+                        userInfoType: .preferredPlaces,
+                        inputText: self.$viewModel.state.preferredPlaces
+                    )
+                )
             }
             if self.viewModel.state.profileSettingPhase.phaseStep > 3 {
-                Spacer().frame(height: 38)
-                Text("관심사")
-                Spacer().frame(height: 10)
-                InputInfoView(
-                    userInfoType: .inserests,
-                    inputText: self.$viewModel.state.interests)
+                infoField(
+                    title: "관심사",
+                    view: InputInfoView(
+                        userInfoType: .inserests,
+                        inputText: self.$viewModel.state.interests
+                    )
+                )
             }
             if self.viewModel.state.profileSettingPhase.phaseStep > 2 {
-                Spacer().frame(height: 38)
-                Text("전화번호")
-                Spacer().frame(height: 10)
-                InputInfoView(
-                    userInfoType: .phoneNumber,
-                    inputText: self.$viewModel.state.phoneNumber)
+                infoField(
+                    title: "전화번호",
+                    view: InputInfoView(
+                        userInfoType: .phoneNumber,
+                        inputText: self.$viewModel.state.phoneNumber
+                    )
+                )
             }
             if self.viewModel.state.profileSettingPhase.phaseStep > 1 {
-                Spacer().frame(height: 38)
-                Text("희망 분야")
-                Spacer().frame(height: 10)
-                SelectPreferredFieldView(
-                    selectedPreferredField: self.$viewModel.state
-                        .preferredField
+                infoField(
+                    title: "희망 분야",
+                    view: SelectPreferredFieldView(
+                        selectedPreferredField: self.$viewModel.state
+                            .preferredField
+                    )
                 )
             }
         }
+    }
+    
+    @ViewBuilder
+    private func infoField(
+        title: String,
+        view: some View
+    ) -> some View {
+        Spacer().frame(height: 38)
+        Text(title).font(DiveFont.bodyMedium1)
+        Spacer().frame(height: 10)
+        view
     }
 }
