c04e98c42f0d063c8b356b24d6e68acf84b2e9ca
pearhyunjin <pearhyunjin@gmail.com>
2025-04-23 18:53:58 +0900

    [feat] 대화 시작을 위한 질문 카드 화면 구현

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/ConversationView/View/ConversationView.swift
@@ -0,0 +1,51 @@
+//
+//  ConversationView.swift
+//  DiverBook_iOS
+//
+//  Created by 배현진 on 4/23/25.
+//
+
+import SwiftUI
+
+struct ConversationView: View {
+    @StateObject var viewModel: ConversationViewModel
+    
+    init(coordinator: Coordinator) {
+        _viewModel = StateObject(wrappedValue: ConversationViewModel(coordinator: coordinator))
+    }
+    
+    let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 0), count: 2)
+    
+    var body: some View {
+        VStack {
+            TopBar()
+            Spacer()
+            Text("아래 카드를 고르면\n다이버와 깊이 잠수해볼 수 있어요")
+                .font(DiveFont.headingH3)
+                .foregroundColor(DiveColor.gray4)
+                .padding(.bottom, 25)
+                .multilineTextAlignment(.center)
+            
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(0..<4) { index in
+                    QuestionCardCell()
+                }
+            }
+            
+            Spacer()
+            PrimaryButton(title: "대화 완료", coordinator: Coordinator()) {
+                // TODO: - 대화 완료 클릭시 화면 전환 (ProfileView로)
+            }
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+struct QuestionCardCell: View {
+    var body: some View {
+        ZStack {
+            Image("questionCard")
+                .shadow(color: DiveColor.gray4.opacity(0.18), radius: 5, y: 5)
+        }
+    }
+}
---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/ConversationView/ViewModel/ConversationViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ConversationViewModel.swift
+//  DiverBook_iOS
+//
+//  Created by 배현진 on 4/23/25.
+//
+
+import Combine
+import SwiftUI
+
+final class ConversationViewModel: ViewModelable {
+    struct State {
+        
+    }
+    
+    enum Action {
+        case finishConversation
+    }
+    
+    @Published var state: State = State()
+    @ObservedObject var coordinator: Coordinator
+    
+    init(coordinator: Coordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func action(_ action: Action) {
+    }
+}
