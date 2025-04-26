20a026e0c53d24ce9ba37e81300ae08fc964a4f3
Jun <wnsgur4092@gmail.com>
2025-04-27 03:44:41 +0900

    chore: App  테스트를 위한 임시 변경 복구

--- DiverBook_iOS/DiverBook_iOS/Sources/Application/DiverBookIOSApp.swift
+++ DiverBook_iOS/DiverBook_iOS/Sources/Application/DiverBookIOSApp.swift
@@ -8,33 +8,32 @@ struct DiverBookIOSApp: App {
 
     var body: some Scene {
         WindowGroup {
-            MyProfileView(viewModel: MyProfileViewModel())
-//            NavigationStack(path: self.$coordinator.path) {
-//                OnboardingView(coordinator: self.coordinator)
-//                    .navigationDestination(
-//                        for: Path.self,
-//                        destination: { path in
-//                            switch path {
-//                            case .splash:
-//                                OnboardingView(coordinator: self.coordinator)
-//                                    .toolbar(.hidden, for: .navigationBar)
-//                            case .userProfileSetting(let nickname):
-//                                UserProfileSettingView(nickname: nickname, coordinator: self.coordinator)
-//                                    .toolbar(.hidden, for: .navigationBar)
-//                            case .idCardScan:
-//                                IDCardScannerView(coordinator: self.coordinator)
-//                                    .toolbar(.hidden, for: .navigationBar)
-//                            case .mainTab:
-//                                DiverBookTabView(coordinator: self.coordinator)
-//                                    .toolbar(.hidden, for: .navigationBar)
-//                            case .searchingDiver:
-//                                DiverSearchingView(coordinator: self.coordinator)
-//                                    .toolbar(.hidden, for: .navigationBar)
-//                            case .searchResult(nickname: let nickname):
-//                                DiverSearchResultView(nickname: nickname)
-//                            }
-//                        })
-//            }
+            NavigationStack(path: self.$coordinator.path) {
+                OnboardingView(coordinator: self.coordinator)
+                    .navigationDestination(
+                        for: Path.self,
+                        destination: { path in
+                            switch path {
+                            case .splash:
+                                OnboardingView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .userProfileSetting(let nickname):
+                                UserProfileSettingView(nickname: nickname, coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .idCardScan:
+                                IDCardScannerView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .mainTab:
+                                DiverBookTabView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .searchingDiver:
+                                DiverSearchingView(coordinator: self.coordinator)
+                                    .toolbar(.hidden, for: .navigationBar)
+                            case .searchResult(nickname: let nickname):
+                                DiverSearchResultView(nickname: nickname)
+                            }
+                        })
+            }
         }
     }
 }
