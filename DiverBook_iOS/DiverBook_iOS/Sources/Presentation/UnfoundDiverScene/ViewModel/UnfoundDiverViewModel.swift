c029b75ec1e3bea8dd4922ac1cd41c2715626611
pearhyunjin <pearhyunjin@gmail.com>
2025-04-27 16:29:51 +0900

    [feat] 미발견 프로필 뷰 구현

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/UnfoundDiverScene/ViewModel/UnfoundDiverViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  UnfoundDiverViewModel.swift
+//  DiverBook_iOS
+//
+//  Created by 배현진 on 4/27/25.
+//
+import Combine
+import SwiftUI
+
+final class UnfoundDiverViewModel: ViewModelable {
+    
+    struct State {
+        
+    }
+    
+    enum Action {
+    }
+    
+    @Published var state: State = State()
+    @ObservedObject var coordinator: Coordinator
+    
+    init(coordinator: Coordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func action(_ action: Action) {
+        
+    }
+}
