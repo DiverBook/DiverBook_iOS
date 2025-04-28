b3a0608441bc2bac85499e9931dd73d19f5cb3cf
keon22han <keonheehan@hansung.ac.kr>
2025-04-29 00:36:50 +0900

    feat: HTTPClient 프로토콜 및 기본 request 구현 추가
    - 공통 네트워크 요청을 담당할 HTTPClient 프로토콜 정의
    - URLComponents를 사용하여 URL 구성
    - URLSession을 이용해 비동기 데이터 요청 처리
    - 응답 상태코드에 따른 오류 처리 및 JSON 디코딩 지원

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Data/Network/HTTPClient.swift
@@ -0,0 +1,61 @@
+//
+//  HTTPClient.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/28/25.
+//
+import Foundation
+
+protocol HTTPClient {
+    func request<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async
+        -> Result<T, RequestError>
+}
+
+extension HTTPClient {
+    func request<T: Decodable>(
+        endpoint: Endpoint,
+        responseModel: T.Type
+    ) async -> Result<T, RequestError> {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = endpoint.scheme
+        urlComponents.host = endpoint.host
+        urlComponents.path = endpoint.path
+        guard let url = urlComponents.url else {
+            return .failure(.invalidURL)
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.allHTTPHeaderFields = endpoint.header
+
+        if let body = endpoint.body {
+            request.httpBody = try? JSONSerialization.data(
+                withJSONObject: body as [String: Any], options: [])
+        }
+
+        do {
+            let (data, response) = try await URLSession.shared.data(
+                for: request, delegate: nil)
+            guard let response = response as? HTTPURLResponse else {
+                return .failure(.decode)
+            }
+
+            switch response.statusCode {
+            case 200...299:
+                guard
+                    let decodeResponse = try? JSONDecoder().decode(
+                        responseModel, from: data)
+                else {
+                    return .failure(.decode)
+                }
+                return .success(decodeResponse)
+            case 401:
+                return .failure(.unauthorized)
+            default:
+                return .failure(.unexpectedStatusCode)
+            }
+        } catch {
+            return .failure(.unknown)
+        }
+    }
+}
