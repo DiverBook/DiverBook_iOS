b623cf90fc108194e436c8c40f2c13b327d8159b
keon22han <keonheehan@hansung.ac.kr>
2025-04-29 01:24:20 +0900

    [feat] 회원가입 시 DTO 를 적절한 형태로 가공할 수 있도록 AuthInfo 구조체 구현 (Entity Layer)
    회원가입 시 DTO 를 적절한 형태로 가공할 수 있도록 AuthInfo 구조체를 구현하였습니다.
    
    - 해당 구조체는 Entity 레이어의 외부 레이어에서 사용될 수 있습니다.

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Domain/Entities/AuthInfo.swift
@@ -0,0 +1,13 @@
+//
+//  AuthInfo.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/28/25.
+//
+
+struct AuthInfo {
+    var id: String
+    var accessToken: String
+    var refreshToken: String
+    var tokenType: String
+}
