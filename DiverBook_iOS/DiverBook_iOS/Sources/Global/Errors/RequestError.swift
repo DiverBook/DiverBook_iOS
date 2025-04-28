f3b0d4e997ad06bbc39efb0edd8e13a7fc08a6ef
keon22han <keonheehan@hansung.ac.kr>
2025-04-29 00:38:01 +0900

    [feat] 네트워크 응답 에러 타입 enum 추가
    네트워크 응답 에러 타입 Enum 을 추가하였습니다.

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Global/Errors/RequestError.swift
@@ -0,0 +1,35 @@
+//
+//  RequestError.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/28/25.
+//
+
+enum RequestError: Error {
+    case decode
+    case invalidURL
+    case noResponse
+    case unauthorized
+    case unexpectedStatusCode
+    case errorWithLog(String)
+    case unknown
+
+    var message: String {
+        switch self {
+        case .decode:
+            return "Decode"
+        case .invalidURL:
+            return "Session expired"
+        case .noResponse:
+            return "noResponse"
+        case .unauthorized:
+            return "unauthorized"
+        case .unexpectedStatusCode:
+            return "unexpected"
+        case .errorWithLog(let errorLog):
+            return errorLog
+        case .unknown:
+            return "unknown"
+        }
+    }
+}
