4a51a8573c5e9b1554b36f53de0435a6dd9fe946
keon22han <keonheehan@hansung.ac.kr>
2025-04-29 01:05:05 +0900

    [feat] DefaultAuthRepository, DiverBookAuthServicable, DiverBookTokenServicable 구현 (Repository Layer)
    - DefaultAuthRepository (UseCase 레이어의 AuthRepository 프로토콜 구현체)
    - DiverBookAuthServicable(프로토콜)
    - DiverBookTokenServicable(프로토콜)
    을 구현하였습니다.
    
    DefaultAuthRepository는 UseCase 레이어의 AuthRepository 프로토콜을 구현하여 UseCase <- Repository 의 의존성 방향을 만족합니다.
    
    또한 Repository 레이어에 DiverBookAuthServicable, DiverBookTokenServicable 프로토콜을 구현하여 Data 레이어(해당 Servicable 들의 구현체가 있는 레이어) 와는 Repository <- Data 의 의존성 방향을 만족합니다.

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Data/Repositories/Interfaces/DiverBookAuthServicable.swift
@@ -0,0 +1,18 @@
+//
+//  DiverBookAuthServicable.swift
+//  DiverBook_iOS
+//
+//  Created by 한건희 on 4/29/25.
+//
+
+protocol DiverBookAuthServicable: HTTPClient {
+    func signUp(
+        userName: String,
+        divisions: String,
+        phoneNumber: String,
+        interests: String,
+        places: String,
+        about: String,
+        password: String
+    ) async -> Result<SignUpResModel, RequestError>
+}
