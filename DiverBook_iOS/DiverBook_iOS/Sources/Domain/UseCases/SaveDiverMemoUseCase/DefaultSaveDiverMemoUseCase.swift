b7f79ef6eb48b006ed1c9cf41f9282c213455973
pearhyunjin <pearhyunjin@gmail.com>
2025-05-11 21:34:24 +0900

    [feat] Collections에 새로 발견한 다이버 정보 추가 유즈케이스 구현

---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Domain/UseCases/SaveDiverMemoUseCase/DefaultSaveDiverMemoUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  DefaultSaveDiverMemoUseCase.swift
+//  DiverBook_iOS
+//
+//  Created by 배현진 on 5/11/25.
+//
+
+final class DefaultSaveDiverMemoUseCase: SaveDiverMemoUseCase{
+    private let diverCollectionRepository: DiverCollectionRepository
+    
+    init(diverCollectionRepository: DiverCollectionRepository) {
+        self.diverCollectionRepository
+        = diverCollectionRepository
+    }
+    
+    func executeSaveDiverMemoUseCase(foundUserId: String, memo: String) async -> Result<CollectedDiverInfo, any Error> {
+        return await diverCollectionRepository.saveDiverMemo(foundUserId: foundUserId, memo: memo)
+    }
+}
---
+++ DiverBook_iOS/DiverBook_iOS/Sources/Domain/UseCases/SaveDiverMemoUseCase/SaveDiverMemoUseCase.swift
@@ -0,0 +1,10 @@
+//
+//  SaveDiverMemoUseCase.swift
+//  DiverBook_iOS
+//
+//  Created by 배현진 on 5/11/25.
+//
+
+protocol SaveDiverMemoUseCase{
+    func executeSaveDiverMemoUseCase(foundUserId: String, memo: String) async -> Result<CollectedDiverInfo, Error>
+}
