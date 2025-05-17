5bf14c75bd2f7a2bcd8d07ef52956cf26dd9e5b3
Jun <wnsgur4092@gmail.com>
2025-05-18 04:20:13 +0900

    feat: 메모 단어수 제한 기능 추가

--- DiverBook_iOS/DiverBook_iOS/Sources/Presentation/DiverProfileScene/View/SubView/CustomTextEditor.swift
+++ DiverBook_iOS/DiverBook_iOS/Sources/Presentation/DiverProfileScene/View/SubView/CustomTextEditor.swift
@@ -1,18 +1,17 @@
 import SwiftUI
 
+
 struct CustomTextEditor: View {
     @Binding var text: String
     let placeholder: String = "기록 입력"
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            if text.isEmpty {
+            if text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
                 Text(placeholder)
                     .font(DiveFont.bodyMedium2)
                     .foregroundStyle(Color(DiveColor.gray3))
-                    .padding(
-                        EdgeInsets(top: 18, leading: 18, bottom: 0, trailing: 0)
-                    )
+                    .padding(EdgeInsets(top: 18, leading: 18, bottom: 0, trailing: 0))
             }
 
             TextEditor(text: $text)
@@ -20,20 +19,21 @@ struct CustomTextEditor: View {
                 .padding(12)
                 .foregroundColor(.black)
                 .scrollContentBackground(.hidden)
+                .onChange(of: text) {
+                    if text.count > 140 {
+                        text = String(text.prefix(140))
+                    }
+                }
         }
         .background(Color(DiveColor.color2).opacity(0.4))
         .applyShadow(DiveShadow.shadow1)
         .frame(height: 140)
         .overlay(alignment: .bottomTrailing) {
-            Text("\(text.count)/140")
+            Text("\(text.trimmingCharacters(in: .whitespacesAndNewlines).count)/140")
                 .font(DiveFont.bodyMedium2)
                 .foregroundStyle(Color(DiveColor.gray3))
                 .padding(.trailing, 16)
                 .padding(.bottom, 10)
         }
     }
-}
-
-#Preview {
-    CustomTextEditor(text: .constant(""))
 }
